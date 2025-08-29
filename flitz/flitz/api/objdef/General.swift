3ed71023a7e54f2daa6c3c7eb945c22139387542
Gyuhwan Park★ <unstabler@unstabler.pl>
2025-08-29 17:34:53 +0900

    api/objdef: SimpleResponse의 필드 정의를 서버와 맞춤

--- flitz/flitz/api/objdef/General.swift
+++ flitz/flitz/api/objdef/General.swift
@@ -13,5 +13,13 @@ struct Ditch: Codable {
 
 struct SimpleResponse: Codable {
     let is_success: Bool
+    
+    /// 오류가 발생한 경우, 그 이유를 설명하는 문자열입니다. (as translation key)
     let reason: String?
+    
+    /// 추가적인 데이터가 있는 경우, key-value 쌍으로 제공됩니다.
+    /// key-value 쌍의 값은 모두 문자열로 제공됩니다.
+    let additional_data: [String: String]?
+    /// 성공했지만 추가적인 메시지가 있는 경우, 그 메시지가 제공됩니다. (as translation key)
+    let additional_message: String?
 }
