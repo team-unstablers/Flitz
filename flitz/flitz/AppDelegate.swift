317457f5e56eb1fa3767dda79068a4351699d311
Gyuhwan Park★ <unstabler@unstabler.pl>
2025-08-31 14:14:15 +0900

    Sentry 구성 추가₩

--- flitz/flitz/AppDelegate.swift
+++ flitz/flitz/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import Sentry
+
 import UIKit
 
 class AppDelegate: NSObject, UIApplicationDelegate {
@@ -14,6 +16,36 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     static fileprivate(set) var apnsToken: String? = nil
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        #if DEBUG
+        logger.info("debug build, sentry disabled")
+        #else
+        SentrySDK.start { options in
+            options.dsn = "https://0db30a3cf6f155e784a36ee4a3deed90@o576637.ingest.us.sentry.io/4509936951296000"
+            options.debug = false // Enabled debug when first installing is always helpful
+
+            // Adds IP for users.
+            // For more information, visit: https://docs.sentry.io/platforms/apple/data-management/data-collected/
+            options.sendDefaultPii = true
+
+            // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+            // We recommend adjusting this value in production.
+            options.tracesSampleRate = 0.2
+
+            // Configure profiling. Visit https://docs.sentry.io/platforms/apple/profiling/ to learn more.
+            options.configureProfiling = {
+                $0.sessionSampleRate = 0.1 // We recommend adjusting this value in production.
+                $0.lifecycle = .trace
+            }
+
+            // Uncomment the following lines to add more data to your events
+            // options.attachScreenshot = true // This adds a screenshot to the error events
+            // options.attachViewHierarchy = true // This adds the view hierarchy to the error events
+            
+            // Enable experimental logging features
+            options.experimental.enableLogs = true
+        }
+        #endif
+
         application.registerForRemoteNotifications()
         
         UserDefaults.standard.register(defaults: [
