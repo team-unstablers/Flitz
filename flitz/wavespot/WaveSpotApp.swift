d883ea54c918e57f33d58a2d6b24d4d3e01963c0
Gyuhwan Park★ <unstabler@unstabler.pl>
2025-09-13 05:59:49 +0900

    WaveSpot을 위한 App Clip 타겟 추가

---
+++ flitz/wavespot/WaveSpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flitz_WaveSpotApp.swift
+//  Flitz WaveSpot
+//
+//  Created by Gyuhwan Park on 9/13/25.
+//
+
+import SwiftUI
+
+@main
+struct WaveSpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
